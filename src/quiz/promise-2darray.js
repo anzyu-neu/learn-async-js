bcbfbbc0cb3ef905a50d5917ad82d8ccb0a4fb82
anzyu-neu <122393965+anzyu-neu@users.noreply.github.com>
2024-02-27 16:27:18 -0500

    notes and added then catch blocks

--- src/quiz/promise-2darray.js
+++ src/quiz/promise-2darray.js
@@ -1,16 +1,20 @@
 function sum2DArray(arr) {
-    return new Promise((resolve, reject) => {
-        console.log('Sum called ... ');
-        if(Array.isArray(arr)) {
-            setTimeout(() => {
+    return new Promise((resolve, reject) => { // takes error components as input
+        // define conditions under which promise is resolved and rejected
+        // creation of the promise does not automically go to the event thread 
+
+        console.log('Sum called ... '); // shows up in the main thread 
+
+        if(Array.isArray(arr)) { // if arr is not an array, immediately reject (else)
+            setTimeout(() => { // setTimeout will put function in the event loop
                 let sum = 0;
                 for (let i = 0; i < arr.length; i++) {
                     for (let j = 0; j < arr[i].length; j++) {
                         sum += arr[i][j];
                     }
-                }
+                } // O(n^2) 
                 console.log('resolving ... ');
-                resolve(sum);
+                resolve(sum); // return from the event loop. sends to then block 
             }, 0);
         }
         else {
@@ -28,8 +32,19 @@ const array2D = [
     [7, 8, 9]
 ];
 
+// code currently doesn't properly handle promise (then catch block)
 const sumPromise1 = sum2DArray(array2D);
-console.log(sumPromise1);
+sumPromise1
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
 
 const sumPromise2 = sum2DArray('array2D');
-console.log(sumPromise2);
+sumPromise2
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+
+/**
+ * array rejection comes first (promise2) because it's faster
+ * promise1 takes more time given the O(n^2) calculation
+ * Creating new promises does not auto go to the event thread 
+ */
