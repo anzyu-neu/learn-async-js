a09b52f499ce819ee2e126054e95bd9f56cb9f47
anzyu-neu <122393965+anzyu-neu@users.noreply.github.com>
2024-02-27 17:35:07 -0500

    implemented the code

--- src/quiz/promise-neg.js
+++ src/quiz/promise-neg.js
@@ -3,3 +3,30 @@ const array2D = [
     [4, 5, 6],
     [7, 8, -9]
 ];
+
+function negsPerRow(arr, rowIdx) {
+    return new Promise((resolve, reject) => {
+        if (arr.length > rowIdx) {
+            const negatives = arr[rowIdx].filter(num => num < 0);
+            if (negatives.length > 0) {
+                resolve({negatives});
+            } else {
+                reject(`No negative numbers in row ${rowIdx}`);
+            }
+        } else {
+            reject("Invalid row index");
+        }
+    })
+}
+
+const negsPerRowPromises = [];
+
+for (let x = 0; x < array2D.length; x++) {
+    negsPerRowPromises.push(negsPerRow(array2D, x));
+}
+
+Promise.any(negsPerRowPromises)
+    .then((result) => {
+        console.log(result);
+    })
+    .catch((error) => console.log(`Error Msg: ${error}`));
