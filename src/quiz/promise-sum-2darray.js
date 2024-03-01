97ec6f2140d4b8e0605fac32464ec3a9f022c73f
anzyu-neu <122393965+anzyu-neu@users.noreply.github.com>
2024-03-01 16:00:19 -0500

    modified for async await

--- src/quiz/promise-sum-2darray.js
+++ src/quiz/promise-sum-2darray.js
@@ -4,7 +4,7 @@ const array2D = [
     [7, 8, 9]
 ];
 
-function sumOfARow( arr, rowIdx) {
+function sumOfARow(arr, rowIdx) {
     return new Promise((resolve, reject) => {
         if (arr.length > rowIdx) {
             setTimeout(() => {
@@ -13,24 +13,36 @@ function sumOfARow( arr, rowIdx) {
                     sum += arr[rowIdx][i];
                 }
                 resolve(sum);
-            })
+            }, 0);
         } else {
             reject("Invalid row index");
         }
     });
 }
 
-rowSumPromises = [];
-
-for (let x= 0; x < array2D.length; x++) {
-    rowSumPromises.push(sumOfARow(array2D, x));
-}
-Promise.all(rowSumPromises)
-    .then((rowSums) => {
+async function calculateSum() {
+    console.log("Calculating sum");
+    rowSumPromises = [];
+    for (let x= 0; x < array2D.length; x++) {
+        rowSumPromises.push(sumOfARow(array2D, x));
+    }
+    try {
+        const rowSums = await Promise.all(rowSumPromises);
         let sum = 0;
         rowSums.forEach(rowSum => {
             sum += rowSum;
         });
-        console.log( `Sum = ${sum}`);
-    })
-    .catch((error) => console.log(`Error Msg: ${error}`));
+    }
+    catch (error) {
+        console.log(`Error Msg: ${error}`);
+        return 'failed';
+    }
+}
+
+calculateSum().then((status) => console.log(status));
+
+// this is the equivalent to above
+// async function main() {
+//     const result = await calculateSum();
+//     console.log(result);
+// }
